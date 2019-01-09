b5e967d72e4ff2084af9990e45474986bec9c480
1111 <67597994@qq.com>
2019-01-09 11:44:11 +0800

    2019-1-9
    0109

---
+++ 111.c
@@ -0,0 +1,29 @@
+#include<reg51.h>
+void main()
+{
+
+
+
+//BAUD
+TMOD=0X20;
+TH1=0XFD;
+TL1=0XFD;
+TR1=1;
+//seial
+SM0=0;
+SM1=1;
+REN=1;
+ES=1;
+EA=1;
+while(1);
+}
+
+void serial() interrupt 4
+{      
+	 if(RI==1)		
+	{
+	   P1=SBUF;
+	   while(!RI);
+	   RI=0;
+}	 
+}
